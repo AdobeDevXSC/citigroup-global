23a97f90d082611144452e0b78e2b632a4210462
Lamont Crook <lamont@adobe.com>
2024-06-09 09:51:48 -0600

    update accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -9,25 +9,25 @@ function hasWrapper(el) {
 }
 
 export default function decorate(block) {
-  console.log(block);
-  [...block.children].forEach((row) => {
-    const label = row.children[0];
-    const summary = document.createElement('summary');
-    summary.className = 'accordion-item-label';
-    summary.append(...label.childNodes);
-    if (!hasWrapper(summary)) {
-      summary.innerHTML = `<p>${summary.innerHTML}</p>`;
-    }
-    // decorate accordion item body
-    const body = row.children[1];
-    body.className = 'accordion-item-body';
-    if (!hasWrapper(body)) {
-      body.innerHTML = `<p>${body.innerHTML}</p>`;
-    }
-    // decorate accordion item
-    const details = document.createElement('details');
-    details.className = 'accordion-item';
-    details.append(summary, body);
-    row.replaceWith(details);
-  });
+  // console.log(block);
+  // [...block.children].forEach((row) => {
+  //   const label = row.children[0];
+  //   const summary = document.createElement('summary');
+  //   summary.className = 'accordion-item-label';
+  //   summary.append(...label.childNodes);
+  //   if (!hasWrapper(summary)) {
+  //     summary.innerHTML = `<p>${summary.innerHTML}</p>`;
+  //   }
+  //   // decorate accordion item body
+  //   const body = row.children[1];
+  //   body.className = 'accordion-item-body';
+  //   if (!hasWrapper(body)) {
+  //     body.innerHTML = `<p>${body.innerHTML}</p>`;
+  //   }
+  //   // decorate accordion item
+  //   const details = document.createElement('details');
+  //   details.className = 'accordion-item';
+  //   details.append(summary, body);
+  //   row.replaceWith(details);
+  // });
 }
