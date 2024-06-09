6ac6fcfcae13576fbdb1f48cd4faaf60fc17a5ec
Lamont Crook <lamont@adobe.com>
2024-06-09 07:35:42 -0600

    merge cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,8 +1,132 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
+import { fetchPlaceholders } from '../../scripts/aem.js';
+import { createHTML } from './helper-function.js';
+
+function updateActiveSlide(block) {
+  const slides = block.querySelectorAll('.carousel-slide');
+  const scrollPosition = block.querySelector('.carousel-slides').scrollLeft;
+  const slideIndex = Math.round(scrollPosition / slides[0].offsetWidth);
+  block.dataset.activeSlide = slideIndex;
+
+  slides.forEach((aSlide, idx) => {
+    aSlide.setAttribute('aria-hidden', idx !== slideIndex);
+    aSlide.querySelectorAll('a').forEach((link) => {
+      if (idx !== slideIndex) {
+        link.setAttribute('tabindex', '-1');
+      } else {
+        link.removeAttribute('tabindex');
+      }
+    });
+  });
+
+  const indicators = block.querySelectorAll('.carousel-slide-indicator');
+  indicators.forEach((indicator, idx) => {
+    if (idx !== slideIndex) {
+      indicator.querySelector('button').removeAttribute('disabled');
+    } else {
+      indicator.querySelector('button').setAttribute('disabled', 'true');
+    }
+  });
+}
+
+function showSlide(block, slideIndex = 0) {
+  const slides = block.querySelectorAll('.carousel-slide');
+  let realSlideIndex = slideIndex < 0 ? slides.length - 1 : slideIndex;
+  if (slideIndex >= slides.length) realSlideIndex = 0;
+  const activeSlide = slides[realSlideIndex];
+
+  activeSlide.querySelectorAll('a').forEach((link) => link.removeAttribute('tabindex'));
+  block.querySelector('.carousel-slides').scrollTo({
+    top: 0,
+    left: activeSlide.offsetWidth * realSlideIndex,
+    behavior: 'smooth',
+  });
+
+  setTimeout(() => {
+    updateActiveSlide(block); // Ensure the active slide is updated after scrolling
+  }, 500); // Adjust delay as necessary
+}
+
+function bindEvents(block) {
+  const slideIndicators = block.querySelector('.carousel-slide-indicators');
+  if (!slideIndicators) return;
+
+  slideIndicators.querySelectorAll('button').forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const slideIndicator = e.currentTarget.parentElement;
+      showSlide(block, parseInt(slideIndicator.dataset.targetSlide, 10));
+    });
+  });
+
+  block.querySelector('.slide-prev').addEventListener('click', () => {
+    showSlide(block, parseInt(block.dataset.activeSlide, 10) - 1);
+  });
+  block.querySelector('.slide-next').addEventListener('click', () => {
+    showSlide(block, parseInt(block.dataset.activeSlide, 10) + 1);
+  });
+
+  const slideObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        updateActiveSlide(block);
+      }
+    });
+  }, { threshold: 0.5 }); // Adjust threshold as needed
+  
+  block.querySelectorAll('.carousel-slide').forEach((slide) => {
+    slideObserver.observe(slide);
+  });
+}
+
+function createSlide(row, slideIndex, carouselId) {
+  const slide = document.createElement('li');
+  slide.dataset.slideIndex = slideIndex;
+  slide.setAttribute('id', `carousel-${carouselId}-slide-${slideIndex}`);
+  slide.classList.add('carousel-slide');
+
+  row.querySelectorAll(':scope > div').forEach((column, colIdx) => {
+    column.classList.add(`carousel-slide-${colIdx === 0 ? 'image' : 'content'}`);
+    const isColumn = column.classList.contains('carousel-slide-content');
+
+    if (isColumn) {
+      const infoDiv = document.createElement('div');
+      infoDiv.className = 'info';
+
+      while (column.firstChild) {
+        infoDiv.appendChild(column.firstChild);
+      }
+
+      column.appendChild(infoDiv);
+    }
+
+    slide.append(column);
+  });
+
+  const labeledBy = slide.querySelector('h1, h2, h3, h4, h5, h6');
+  if (labeledBy) {
+    slide.setAttribute('aria-labelledby', labeledBy.getAttribute('id'));
+  }
+
+  const contentWrapper = slide.querySelector('.carousel-slide-content');
+  const link = contentWrapper.querySelector('a');
+  const href = link?.href;
+
+  if (href) {
+    const newLink = document.createElement('a');
+    newLink.href = href;
+
+    while (slide.firstChild) {
+      newLink.appendChild(slide.firstChild);
+      link.parentElement.remove();
+    }
+
+    slide.appendChild(newLink);
+  }
+
+  return slide;
+}
 
 const aemInstance = 'https://author-p101152-e938206.adobeaemcloud.com';
-
+let carouselId = 0;
 export default async function decorate(block) {
   const obj = {
     method: 'get',
