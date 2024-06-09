0ec01ad461f5a21bc6361e1ec26d741de6fb63f7
Lamont Crook <lamont@adobe.com>
2024-06-09 08:30:54 -0600

    add accordion

--- blocks/cards/helper-function.js
+++ blocks/cards/helper-function.js
@@ -1,4 +1,4 @@
-// import { createOptimizedPicture } from '../../scripts/aem.js';
+import { createOptimizedPicture } from '../../scripts/aem.js';
 
 /**
  * Returns a picture element with webp and fallbacks
@@ -8,48 +8,48 @@
  * @param {Array} [breakpoints] Breakpoints and corresponding params (eg. width)
  * @returns {Element} The picture element
  */
-function createOptimizedPicture(
-  src,
-  alt = '',
-  eager = false,
-  breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }],
-) {
-  const url = new URL(src, window.location.href);
-  const picture = document.createElement('picture');
-  let { pathname } = url;
-  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+// function createOptimizedPicture(
+//   src,
+//   alt = '',
+//   eager = false,
+//   breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }],
+// ) {
+//   const url = new URL(src, window.location.href);
+//   const picture = document.createElement('picture');
+//   let { pathname } = url;
+//   const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
 
-  pathname = pathname.replace('/content/citigroup', '');
+//   pathname = pathname.replace('/content/citigroup', '');
 
-  // webp
-  breakpoints.forEach((br) => {
-    const source = document.createElement('source');
-    if (br.media) source.setAttribute('media', br.media);
-    // source.setAttribute('type', 'image/webp');
-    // source.srcset = '';//`${url}?width=${br.width}&format=webply&optimize=medium`;
-    source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
-    picture.appendChild(source);
-    console.log(source);
-  });
+//   // webp
+//   breakpoints.forEach((br) => {
+//     const source = document.createElement('source');
+//     if (br.media) source.setAttribute('media', br.media);
+//     // source.setAttribute('type', 'image/webp');
+//     // source.srcset = '';//`${url}?width=${br.width}&format=webply&optimize=medium`;
+//     source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
+//     picture.appendChild(source);
+//     console.log(source);
+//   });
 
-  // fallback
-  breakpoints.forEach((br, i) => {
-    if (i < breakpoints.length - 1) {
-      const source = document.createElement('source');
-      if (br.media) source.setAttribute('media', br.media);
-      source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
-      picture.appendChild(source);
-    } else {
-      const img = document.createElement('img');
-      img.setAttribute('loading', eager ? 'eager' : 'lazy');
-      img.setAttribute('alt', alt);
-      picture.appendChild(img);
-      img.setAttribute('src', `${url}?width=${br.width}&format=${ext}&optimize=medium`);
-    }
-  });
+//   // fallback
+//   breakpoints.forEach((br, i) => {
+//     if (i < breakpoints.length - 1) {
+//       const source = document.createElement('source');
+//       if (br.media) source.setAttribute('media', br.media);
+//       source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+//       picture.appendChild(source);
+//     } else {
+//       const img = document.createElement('img');
+//       img.setAttribute('loading', eager ? 'eager' : 'lazy');
+//       img.setAttribute('alt', alt);
+//       picture.appendChild(img);
+//       img.setAttribute('src', `${url}?width=${br.width}&format=${ext}&optimize=medium`);
+//     }
+//   });
 
-  return picture;
-}
+//   return picture;
+// }
 
 export function createHTML(articles) {
   const updatedArticles = [];
@@ -58,8 +58,10 @@ export function createHTML(articles) {
   articles.forEach((article) => {
     const optimizedPic = createOptimizedPicture(aemInstance + article.banner.dynamicUrl, article.title, true);
       optimizedPic.querySelectorAll('source').forEach((s) => {
-        s.srcset = aemInstance + s.srcset;
+        // s.srcset = aemInstance + s.srcset;
     });
+
+    console.log(optimizedPic);
 
     updatedArticles.push(`
       <div class="article-container">
